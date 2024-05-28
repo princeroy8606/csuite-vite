89d47498215b5de17c92d33b359e48fed950ae3c
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-05-28 16:48:48 +0530

    Update SignUp.jsx

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -49,7 +49,7 @@ const SignUp = ({ toggleSlide }) => {
   };
 
   return (
-    <div className="w-full h-full bg-white rounded-r-2xl flex flex-col items-center justify-around p-4">
+    <div className="w-full h-full bg-white rounded-r-2xl flex flex-col items-center justify-around pt-8 pb-8">
       <div className="w-20 h-20 rounded-full p-4 bg-light-gray flex items-center justify-center tablet-h:w-14 tablet-h:h-14 laptop-h:w-20 laptop-h:h-20">
         <img
           src={assets.Images.CSuiteLogo}
